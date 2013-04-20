ec51e63d6dd2cde1df6c9b1a5f2422ce86bda220
Gustavo Bragança <ga.braganca@gmail.com>
2013-04-20 00:16:11 -0300

    Change synplot functions into a class.
    Changed all functions related to synplot into a function called
    synplot. This class still uses the IDL synplot, so it is a class
    wrapper.

--- s4/synthesis/__init__.py
+++ s4/synthesis/__init__.py
@@ -1,1 +1,1 @@
-from synplotwrapper import change_spath, synplot_input, run_load
+from synplotwrapper import synplot
--- s4/synthesis/synplotwrapper.py
+++ s4/synthesis/synplotwrapper.py
@@ -5,44 +5,43 @@ from ..idlwrapper import idlwrapper
 from os import getenv
 #=============================================================================
 
-#=============================================================================
-# Global constants
-SYNPLOT_PATH  = getenv('HOME')+'/.s4/synthesis/synplot/'
-#=============================================================================
 
-#=============================================================================
-# Change synplot path
-def change_spath(new_path):
-    """Change synplot path"""
-    global SYNPLOT_PATH
-    SYNPLOT_PATH = new_path
-#=============================================================================
 
-#=============================================================================
-#     
-def synplot_input(**kwargs):
-    """Build the synplot command to IDL.
+class synplot:
+    """Add docstring"""
     
-    It also checks if Teff, logg, wstart and wend were defined.
-    
-    """
-    #Check if some params were defined
-    for par in ['teff', 'logg', 'wstart', 'wend']:
-        if par not in kwargs.keys():
-            raise ValueError(par + ' were not defined.')
-            
-    return "CD, '"+SYNPLOT_PATH+"' & synplot, "+ \
-       ', '.join([key+' = '+str(value) for key, value in kwargs.iteritems()])
-#============================================================================= 
+    def __init__(self, synplot_path = None, **kwargs):
+        if synplot_path is None:
+            self.path = getenv('HOME')+'/.s4/synthesis/synplot/'
+        else:
+            self.path = synplot_path
+        
+        #Check if some params were defined    
+        for par in ['teff', 'logg', 'wstart', 'wend']:
+            if par not in kwargs.keys():
+                raise ValueError(par + ' were not defined.')    
+        
+        self.parameters = kwargs
 
-#=============================================================================
-# Run synplot and return the computed spectra
-def run_load(synplot_inp):
-    '''Run synplot and return the computed spectra'''
+    #=============================================================================
+    #     
+    def synplot_input(self):
+        """Build the synplot command to IDL."""
+        
+        synplot_command = [key+' = '+str(value)                              \
+                           for key, value in self.parameters.iteritems()]      
+                
+        return "CD, '"+self.path+"' & synplot, "+ \
+                        ', '.join(synplot_command)
+    #============================================================================= 
     
-    #Run Synplot
-    idlwrapper.run_idl(synplot_inp, do_log = True)
-
-    #load synthetized spectra
-    return loadtxt(SYNPLOT_PATH + 'fort.11')    
-#==============================================================================
+    #=============================================================================
+    # Run synplot and return the computed spectra
+    def run(self):
+        '''Run synplot and store the computed spectra'''
+ 
+        idlwrapper.run_idl(self.synplot_input(), do_log = True)
+    
+        #load synthetized spectra
+        self.spectra = loadtxt(self.path + 'fort.11')    
+    #==============================================================================
