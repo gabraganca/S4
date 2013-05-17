4d5222d711cdff1b6274fa74435bf22339296c35
Gustavo Braganca <ga.braganca@gmail.com>
2013-05-17 20:17:30 -0300

    Button works and call a synplot test method.

--- synplot_gui/synGUI.py
+++ synplot_gui/synGUI.py
@@ -11,7 +11,15 @@ I am following the tutorial found at http://zetcode.com/tutorials/pyqt4/
 # Import Modules
 import sys
 from PyQt4 import QtGui
+import s4
 
+#==============================================================================
+
+#==============================================================================
+# synplot test module
+def synplot_test():
+    syn = s4.synthesis.synplot(20000, 4, wstart = 4460, wend = 4480)
+    syn.plot()
 #==============================================================================
 
 #==============================================================================
@@ -31,11 +39,13 @@ class Widget(QtGui.QWidget):
         # set font for tips
         QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
         
-        # an temporary empty button
-        btn = QtGui.QPushButton('Run', self)
-        btn.setToolTip('Press to run <b>synplot</b> <b>(INACTIVE)</b>')
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)
+        # button to run synplot
+        run_btn = QtGui.QPushButton('Run', self)
+        run_btn.clicked.connect(synplot_test)
+        run_btn.setToolTip('Press to run <b>synplot</b> <b>(INACTIVE)</b>')
+        run_btn.resize(run_btn.sizeHint())
+        run_btn.move(50, 50)
+        
     
         self.show()
 #==============================================================================
