185c84d948ea4950c699df78fd868f425f2f04e4
Gustavo Braganca <ga.braganca@gmail.com>
2013-09-30 15:33:47 +0200

    Sketch script
    This script will allow me to see how a spectral line of wavelength range
    vary with a stellar parameter.

---
+++ scripts/sensi_line.py
@@ -0,0 +1,41 @@
+"""
+The purpose of this sciprt is to make a nimated plot of how a stellar
+parameter change on a spectral line or wavelength coverage.
+"""
+
+import s4
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def main(v_param, values, wstart, wend, spath = None):
+    """
+    Parameters
+    ---------
+
+    v_param: str;
+        Stelllar parameter to be changed.
+
+    values: array;
+        Array of values to plot the parameter.
+
+    wstart: float;
+        Beginning wavelength.
+
+    wend: float;
+        Ending wavelength.
+    """
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+    v_param - sys.argv[1]
+    wstart = sys.argv[2]
+    wend = syns.argv[3]
+
