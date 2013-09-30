0f22e9df289cc38d711552f9aad96a3437bf63f8
Gustavo Braganca <ga.braganca@gmail.com>
2013-09-30 18:19:36 +0200

    Uncomment line to save animation.
    In order for the animation to work, I had to install mencoder AND
    ffmpeg, but I think that was the ffmpeg that made it work.

--- scripts/sensi_line.py
+++ scripts/sensi_line.py
@@ -79,8 +79,7 @@ def main(v_param, values, wstart, wend, spath = None):
     # the video can be embedded in html5.  You may need to adjust this for
     # your system: for more information, see
     # http://matplotlib.sourceforge.net/api/animation_api.html
-#    anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-
+    anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
     plt.show()
 
 
