61d9ef557e54536373ddbde5857829db85383722
Gustavo Braganca <ga.braganca@gmail.com>
2014-06-16 00:19:02 +0200

    Correct path to Periodic table
    The Periodic table is now stored on a special folder under ~/.s4

--- s4/fitting/synfit.py
+++ s4/fitting/synfit.py
@@ -24,7 +24,7 @@ from ..synthesis import Synplot
 from ..spectools import rvcorr
 
 # Loads the chemical elements and their atomic numbers
-PERIODIC = json.load(open(os.path.dirname(__file__)+\
+PERIODIC = json.load(open(os.getenv('HOME')+'/.s4/extra_resc'+\
                           '/chemical_elements.json'))
 
 REVERSE_PERIODIC = {val:key for key, val in PERIODIC.iteritems()}
