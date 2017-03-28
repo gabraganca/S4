7fb6324a07d81b90866a72739ec5244e8f0707e3
Gustavo Braganca <ga.braganca@gmail.com>
2017-03-28 12:09:37 -0300

    Improve some tests on Synplot

--- tests/test_synplot.py
+++ tests/test_synplot.py
@@ -1,5 +1,7 @@
 """Test suite for Synplot"""
+from __future__ import print_function
 import os
+import numpy as np
 from s4.synthesis import Synplot
 
 
@@ -11,18 +13,23 @@ PARAMS = dict(wstart=4000, wend=4500, noplot=True, relative=1)
 def test_synplot_run():
     """Test if Synplot is calculating a spectrum"""
     try:
-        Synplot(TEFF, LOGG, **PARAMS).run()
+        syn = Synplot(TEFF, LOGG, **PARAMS)
+        syn.run()
     except:
         # Print the output log of the Synplot command
-        with open('run.log') as f:
-            print (f.read())
+        with open('run.log') as log:
+            print(log.read())
         raise
+
+    assert isinstance(syn.spectrum, np.ndarray)
+
 
 def test_synplot_save():
     """Test if Synplot is saving a spectrum"""
     fname = '/tmp/test.spec'
 
-    _ = Synplot(TEFF, LOGG, **PARAMS)
-    _.save_spec(fname)
+    syn = Synplot(TEFF, LOGG, **PARAMS)
+    syn.run()
+    syn.save_spec(fname)
 
     assert os.path.isfile(fname)
